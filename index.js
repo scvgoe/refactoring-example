8529c4183155cb5ec772994184cb7d5c99a26c4d
DaeseongKim <scvgoe@gmail.com>
2022-08-16 06:03:34 +0900

    Extract format

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = function statement(invoice, plays) {
 	let totalAmount = 0;
 	let volumeCredits = 0;
 	let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-	const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
 	for (let perf of invoice.performances) {
 		volumeCredits += volumeCreditsFor(perf);
@@ -14,6 +13,10 @@ module.exports = function statement(invoice, plays) {
 	result += `총액: ${format(totalAmount / 100)}\n`;
 	result += `적립 포인트: ${volumeCredits}점\n`;
 	return result;
+
+	function format(aNumber) {
+		return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber);
+	}
 
 	function volumeCreditsFor(aPerformance) {
 		// 포인트를 적립한다.
