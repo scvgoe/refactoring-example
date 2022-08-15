6285eff45f7677e1912670499aae569c67fc5384
DaeseongKim <scvgoe@gmail.com>
2022-08-16 06:01:19 +0900

    Rename perf in volumCreditsFor

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ module.exports = function statement(invoice, plays) {
 	result += `적립 포인트: ${volumeCredits}점\n`;
 	return result;
 
-	function volumeCreditsFor(perf) {
+	function volumeCreditsFor(aPerformance) {
 		// 포인트를 적립한다.
-		let result = Math.max(perf.audience - 30, 0);
+		let result = Math.max(aPerformance.audience - 30, 0);
 		// 희극 관객 5명마다 추가 포인트를 제공한다.
-		if ("comedy" === playFor(perf).type)
-			result += Math.floor(perf.audience / 5);
+		if ("comedy" === playFor(aPerformance).type)
+			result += Math.floor(aPerformance.audience / 5);
 
 		return result;
 	}
