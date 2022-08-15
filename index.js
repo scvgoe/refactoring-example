b45cdc5ad1386d770c3e2d0ec4d8de068ee138df
DaeseongKim <scvgoe@gmail.com>
2022-08-16 06:14:29 +0900

    Extract totalAmount function

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ module.exports = function statement(invoice, plays) {
 		result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
 	}
 
-	let totalAmount = 0;
-	for (let perf of invoice.performances) {
-		totalAmount += amountFor(perf);
-	}
+	let totalAmount = appleSauce();
 
 	result += `총액: ${usd(totalAmount)}\n`;
 	result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 	return result;
+
+	function appleSauce() {
+		let totalAmount = 0;
+		for (let perf of invoice.performances) {
+			totalAmount += amountFor(perf);
+		}
+		return totalAmount;
+	}
 
 	function totalVolumeCredits() {
 		let volumeCredits = 0;
