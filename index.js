67950553677cccbb8d6ac68a50e557b69c70a209
DaeseongKim <scvgoe@gmail.com>
2022-08-16 06:15:53 +0900

    Rename inner function variable

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ module.exports = function statement(invoice, plays) {
 	return result;
 
 	function totalAmount() {
-		let totalAmount = 0;
+		let result = 0;
 		for (let perf of invoice.performances) {
-			totalAmount += amountFor(perf);
+			result += amountFor(perf);
 		}
-		return totalAmount;
+		return result;
 	}
 
 	function totalVolumeCredits() {
-		let volumeCredits = 0;
+		let result = 0;
 		for (let perf of invoice.performances) {
-			volumeCredits += volumeCreditsFor(perf);
+			result += volumeCreditsFor(perf);
 		}
-		return volumeCredits;
+		return result;
 	}
 
 	function usd(aNumber) {
