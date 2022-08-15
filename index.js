2f5e38eebdce7d713b908b75fa88f8d5f921851a
DaeseongKim <scvgoe@gmail.com>
2022-08-16 06:49:21 +0900

    Remove plays from renderPlainText

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = function statement(invoice, plays) {
 	}
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
 	let result = `청구 내역 (고객명: ${data.customer})\n`;
 
 	for (let perf of data.performances) {
