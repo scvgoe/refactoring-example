ae96f42c203bec8e14a10b12e2b00ff146988c58
DaeseongKim <scvgoe@gmail.com>
2022-08-16 05:53:37 +0900

    Inline playFor in amountFor function

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ module.exports = function statement(invoice, plays) {
 	function amountFor(play, aPerformance) {
 		let result = 0;
 
-		switch (play.type) {
+		switch (playFor(aPerformance).type) {
 			case "tragedy": // 비극
 				result = 40000;
 				if (aPerformance.audience > 30) {
@@ -42,7 +42,7 @@ module.exports = function statement(invoice, plays) {
 				result += 300 * aPerformance.audience;
 				break;
 			default:
-				throw new Error(`알 수 없는 장르: ${play.type}`);
+				throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
 		}
 		return result;
 	}
