584f18d20572e74cbf97c949f3afa43de548fa2e
DaeseongKim <scvgoe@gmail.com>
2022-08-16 06:11:34 +0900

    Remove comment

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = function statement(invoice, plays) {
 	let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
 	for (let perf of invoice.performances) {
-		// 청구 내역을 출력한다.
 		result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
 		totalAmount += amountFor(perf);
 	}
@@ -25,9 +24,7 @@ module.exports = function statement(invoice, plays) {
 	}
 
 	function volumeCreditsFor(aPerformance) {
-		// 포인트를 적립한다.
 		let result = Math.max(aPerformance.audience - 30, 0);
-		// 희극 관객 5명마다 추가 포인트를 제공한다.
 		if ("comedy" === playFor(aPerformance).type)
 			result += Math.floor(aPerformance.audience / 5);
 
@@ -42,13 +39,13 @@ module.exports = function statement(invoice, plays) {
 		let result = 0;
 
 		switch (playFor(aPerformance).type) {
-			case "tragedy": // 비극
+			case "tragedy":
 				result = 40000;
 				if (aPerformance.audience > 30) {
 					result += 1000 * (aPerformance.audience - 30);
 				}
 				break;
-			case "comedy": // 희극
+			case "comedy":
 				result = 30000;
 				if (aPerformance.audience > 20) {
 					result += 10000 + 500 * (aPerformance.audience - 20);
