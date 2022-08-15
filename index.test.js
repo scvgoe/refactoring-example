bb73901f467ed69addfaf9614fb67808251e5ef6
DaeseongKim <scvgoe@gmail.com>
2022-08-16 07:16:58 +0900

    Add htmlStatement test

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { statement } = require('./index')
+const { statement, htmlStatement } = require('./index')
 
 describe('statement', () => {
 	let playsJson = require('./plays.json');
@@ -13,5 +13,18 @@ describe('statement', () => {
 			"적립 포인트: 47점\n";
 
 		expect(statement(invoicesJson[0], playsJson)).toBe(expected);
+	});
+
+	it('should print a statement for multiple plays, single customer and multiple seats in html', () => {
+		let expected = "<h1>청구 내역 (고객명: BigCo)</h1>\n" +
+			"<table>\n" +
+			"<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr> <tr><td>Hamlet</td><td>(55석)</td><td>$650.00</td>\n" +
+			" <tr><td>As You Like It</td><td>(35석)</td><td>$580.00</td>\n" +
+			" <tr><td>Othello</td><td>(40석)</td><td>$500.00</td>\n" +
+			"</table>\n" +
+			"<p>총액: <em>$1,730.00</em></p>\n" +
+			"<p>적립 포인트: <em>47</em>점</p>\n";
+
+		expect(htmlStatement(invoicesJson[0], playsJson)).toBe(expected);
 	});
 });
