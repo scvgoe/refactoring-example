9923ea2cac3d3c78a1c3d88291470d4a2e3c6e79
DaeseongKim <scvgoe@gmail.com>
2022-08-16 05:52:10 +0900

    Inline playFor

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ module.exports = function statement(invoice, plays) {
 	const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
 	for (let perf of invoice.performances) {
-		const play = plays[perf.playID];
-		let thisAmount = amountFor(play, perf);
+		let thisAmount = amountFor(playFor(perf), perf);
 
 		// 포인트를 적립한다.
 		volumeCredits += Math.max(perf.audience - 30, 0);
 		// 희극 관객 5명마다 추가 포인트를 제공한다.
-		if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+		if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
 		// 청구 내역을 출력한다.
-		result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+		result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
 		totalAmount += thisAmount;
 	}
 	result += `총액: ${format(totalAmount / 100)}\n`;
