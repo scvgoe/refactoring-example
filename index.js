5bb8079e9613812beb764909b47f1877429eac99
DaeseongKim <scvgoe@gmail.com>
2022-08-16 05:59:34 +0900

    Extract volumeCreditsFor

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ module.exports = function statement(invoice, plays) {
 	const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
 	for (let perf of invoice.performances) {
-		// 포인트를 적립한다.
-		volumeCredits += Math.max(perf.audience - 30, 0);
-		// 희극 관객 5명마다 추가 포인트를 제공한다.
-		if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+		volumeCredits += volumeCreditsFor(perf);
 
 		// 청구 내역을 출력한다.
 		result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
@@ -17,6 +14,16 @@ module.exports = function statement(invoice, plays) {
 	result += `총액: ${format(totalAmount / 100)}\n`;
 	result += `적립 포인트: ${volumeCredits}점\n`;
 	return result;
+
+	function volumeCreditsFor(perf) {
+		// 포인트를 적립한다.
+		let volumeCredits = Math.max(perf.audience - 30, 0);
+		// 희극 관객 5명마다 추가 포인트를 제공한다.
+		if ("comedy" === playFor(perf).type)
+			volumeCredits += Math.floor(perf.audience / 5);
+
+		return volumeCredits;
+	}
 
 	function playFor(aPerformance) {
 		return plays[aPerformance.playID];
