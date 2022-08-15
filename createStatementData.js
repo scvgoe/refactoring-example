a3bc89923eeafadbec1261c6281af83fea273191
DaeseongKim <scvgoe@gmail.com>
2022-08-16 07:32:03 +0900

    Mov volumeCreditsFor into calculator

--- createStatementData.js
+++ createStatementData.js
@@ -12,20 +12,12 @@ exports.createStatementData = function createStatementData(invoice, plays) {
 		const result = Object.assign({}, aPerformance);
 		result.play = playFor(result);
 		result.amount = performanceCalculator.amount;
-		result.volumeCredits = volumeCreditsFor(result);
+		result.volumeCredits = performanceCalculator.volumeCredits;
 		return result;
 	}
 
 	function playFor(aPerformance) {
 		return plays[aPerformance.playID];
-	}
-
-	function volumeCreditsFor(aPerformance) {
-		let result = Math.max(aPerformance.audience - 30, 0);
-		if ("comedy" === aPerformance.play.type)
-			result += Math.floor(aPerformance.audience / 5);
-
-		return result;
 	}
 
 	function totalAmount(data) {
@@ -66,4 +58,11 @@ class PerformanceCalculator {
 		return result;
 	}
 
+	get volumeCredits() {
+		let result = Math.max(this.performance.audience - 30, 0);
+		if ("comedy" === this.play.type)
+			result += Math.floor(this.performance.audience / 5);
+
+		return result;
+	}
 }
