c6dfdd6927a6415baa9a1b28e10f35550037160e
DaeseongKim <scvgoe@gmail.com>
2022-08-16 06:13:58 +0900

    Move separated for loop for totalAmount

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = function statement(invoice, plays) {
-	let totalAmount = 0;
 	let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
 	for (let perf of invoice.performances) {
 		result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
 	}
 
+	let totalAmount = 0;
 	for (let perf of invoice.performances) {
 		totalAmount += amountFor(perf);
 	}
