654d4fa3023cb8066b14592e36c6f888e630929a
DaeseongKim <scvgoe@gmail.com>
2022-08-16 06:13:42 +0900

    Move separated for loop for totalAmount

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ module.exports = function statement(invoice, plays) {
 
 	for (let perf of invoice.performances) {
 		result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+	}
+
+	for (let perf of invoice.performances) {
 		totalAmount += amountFor(perf);
 	}
 
