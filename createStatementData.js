25aeebb58d8e71a03de7da53b099570a694e2d00
DaeseongKim <scvgoe@gmail.com>
2022-08-16 07:43:50 +0900

    Reorder statements

--- createStatementData.js
+++ createStatementData.js
@@ -29,6 +29,14 @@ exports.createStatementData = function createStatementData(invoice, plays) {
 	}
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+	switch (aPlay.type) {
+		case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+		case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+		default: throw new Error(`알 수 없는 장르: ${this.play.type}`);
+	}
+}
+
 class PerformanceCalculator {
 	constructor(aPerformance, aPlay) {
 		this.performance = aPerformance;
@@ -41,14 +49,6 @@ class PerformanceCalculator {
 
 	get volumeCredits() {
 		return Math.max(this.performance.audience - 30, 0);
-	}
-}
-
-function createPerformanceCalculator(aPerformance, aPlay) {
-	switch (aPlay.type) {
-		case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
-		case "comedy": return new ComedyCalculator(aPerformance, aPlay);
-		default: throw new Error(`알 수 없는 장르: ${this.play.type}`);
 	}
 }
 
