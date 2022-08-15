3a1e0b12ba4e28c8161a81cd0bcb291f7fa301e6
DaeseongKim <scvgoe@gmail.com>
2022-08-16 06:14:52 +0900

    Inline totalAmount

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ module.exports = function statement(invoice, plays) {
 		result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
 	}
 
-	let totalAmount = appleSauce();
-
-	result += `총액: ${usd(totalAmount)}\n`;
+	result += `총액: ${usd(appleSauce())}\n`;
 	result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 	return result;
 
