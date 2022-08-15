877fbc340dc3ca0bb3c3d84700bc1e174493f3e9
DaeseongKim <scvgoe@gmail.com>
2022-08-16 07:36:37 +0900

    Add genre calculator

--- createStatementData.js
+++ createStatementData.js
@@ -68,5 +68,17 @@ class PerformanceCalculator {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-	return new PerformanceCalculator(aPerformance, aPlay);
+	switch (aPlay.type) {
+		case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+		case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+		default: throw new Error(`알 수 없는 장르: ${this.play.type}`);
+	}
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+
 }
