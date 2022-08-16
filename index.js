38a5c430492c4c7d2790c38cbfda78783e97d831
DaeseongKim <scvgoe@gmail.com>
2022-08-16 11:58:18 +0900

    Rename appleSauce

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 module.exports = function statement(invoice, plays) {
 	let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
-	let totalAmount = appleSauce();
 	for (let perf of invoice.performances) {
 		// 청구 내역을 출력한다.
 		result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
 	}
 
-	result += `총액: ${usd(totalAmount)}\n`;
+	result += `총액: ${usd(totalAmount())}\n`;
 	result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 	return result;
 
-	function appleSauce() {
-		let totalAmount = 0;
+	function totalAmount() {
+		let result = 0;
 		for (let perf of invoice.performances) {
-			totalAmount += amountFor(perf);
+			result += amountFor(perf);
 		}
-		return totalAmount;
+		return result;
 	}
 
 	function totalVolumeCredits() {
