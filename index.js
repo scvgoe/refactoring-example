e6dac51a80faa1d25340c4d3b0ad3f92ed2871fe
DaeseongKim <scvgoe@gmail.com>
2022-08-16 12:10:43 +0900

    Move amountFor to calculating

--- index.js
+++ index.js
@@ -6,53 +6,12 @@ module.exports = function statement(invoice, plays) {
 
 	function enrichStatementData(aPerformance) {
 		aPerformance.play = playFor(aPerformance)
+		aPerformance.amount = amountFor(aPerformance)
 		return aPerformance;
 	}
 
 	function playFor(aPerformance) {
 		return plays[aPerformance.playID];
-	}
-}
-
-function renderPlainText(data) {
-	let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-	for (let perf of data.performances) {
-		// 청구 내역을 출력한다.
-		result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-	}
-
-	result += `총액: ${usd(totalAmount())}\n`;
-	result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-	return result;
-
-	function totalAmount() {
-		let result = 0;
-		for (let perf of data.performances) {
-			result += amountFor(perf);
-		}
-		return result;
-	}
-
-	function totalVolumeCredits() {
-		let result = 0;
-		for (let perf of data.performances) {
-			// 포인트를 적립한다.
-			result += volumeCreditsFor(perf);
-		}
-		return result;
-	}
-
-	function usd(aNumber) {
-		return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
-	}
-
-	function volumeCreditsFor(aPerformance) {
-		let result = Math.max(aPerformance.audience - 30, 0);
-		// 희극 관객 5명마다 추가 포인트를 제공한다.
-		if ("comedy" === aPerformance.play.type)
-			result += Math.floor(aPerformance.audience / 5);
-		return result;
 	}
 
 	function amountFor(aPerformance) {
@@ -75,6 +34,48 @@ function renderPlainText(data) {
 			default:
 				throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
 		}
+		return result;
+	}
+}
+
+function renderPlainText(data) {
+	let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+	for (let perf of data.performances) {
+		// 청구 내역을 출력한다.
+		result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+	}
+
+	result += `총액: ${usd(totalAmount())}\n`;
+	result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+	return result;
+
+	function totalAmount() {
+		let result = 0;
+		for (let perf of data.performances) {
+			result += perf.amount;
+		}
+		return result;
+	}
+
+	function totalVolumeCredits() {
+		let result = 0;
+		for (let perf of data.performances) {
+			// 포인트를 적립한다.
+			result += volumeCreditsFor(perf);
+		}
+		return result;
+	}
+
+	function usd(aNumber) {
+		return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
+	}
+
+	function volumeCreditsFor(aPerformance) {
+		let result = Math.max(aPerformance.audience - 30, 0);
+		// 희극 관객 5명마다 추가 포인트를 제공한다.
+		if ("comedy" === aPerformance.play.type)
+			result += Math.floor(aPerformance.audience / 5);
 		return result;
 	}
 }
