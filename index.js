f6b641da2b5c11773eeb3ac6bd01b1fbd263b951
DaeseongKim <scvgoe@gmail.com>
2022-08-16 12:20:49 +0900

    Add renderHTml

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { createStatementData } = require("./createStatementData");
 
-module.exports = function statement(invoice, plays) {
+exports.statement = function statement(invoice, plays) {
 	return renderPlainText(createStatementData(invoice, plays));
+}
+
+exports.htmlStatement = function htmlStatement(invoice, plays) {
+	return renderHtml(createStatementData(invoice, plays));
 }
 
 function renderPlainText(data) {
@@ -15,9 +19,23 @@ function renderPlainText(data) {
 	result += `총액: ${usd(data.totalAmount)}\n`;
 	result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
 	return result;
+}
 
+function renderHtml(data) {
+	let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+	result += "<table>\n";
+	result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
 
-	function usd(aNumber) {
-		return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
+	for (let perf of data.performances) {
+		result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td><td>${usd(perf.amount)}</td>\n`;
 	}
+	result += '</table>\n';
+
+	result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+	result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+	return result;
+}
+
+function usd(aNumber) {
+	return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
 }
