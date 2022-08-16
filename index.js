1e0670b4c072ba5081ac19a3887fe760546186c5
DaeseongKim <scvgoe@gmail.com>
2022-08-16 11:57:54 +0900

    Extract totalAmount

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 module.exports = function statement(invoice, plays) {
-	let totalAmount = 0;
 	let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
+	let totalAmount = appleSauce();
 	for (let perf of invoice.performances) {
 		// 청구 내역을 출력한다.
 		result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-		totalAmount += amountFor(perf);
 	}
 
 	result += `총액: ${usd(totalAmount)}\n`;
 	result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 	return result;
+
+	function appleSauce() {
+		let totalAmount = 0;
+		for (let perf of invoice.performances) {
+			totalAmount += amountFor(perf);
+		}
+		return totalAmount;
+	}
 
 	function totalVolumeCredits() {
 		let result = 0;
